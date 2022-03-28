db331dc32d8ad1c965f834f6ca6fc885a24303b2
Priyam Carto <cpriyam90@gmail.com>
2022-03-27 22:32:27 -0400

    right and wrong color added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
+//Define variables to link to HTML elements
 var startButton = document.getElementById("start-btn")
+var nextButton = document.getElementById("next-btn")
 var questionContainer = document.getElementById("questions-container")
 var highscoreButton = document.getElementById("highscore-btn")
 var homePage = document.getElementById("home")
+var questionElement = document.getElementById("question")
+var answerButtonsElement = document.getElementById("answer-buttons")
 
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener("click",startGame)
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+  })
 
+//Create a function to start game
 function startGame (){
     startButton.classList.add("hide")
     highscoreButton.classList.add("hide")
@@ -15,16 +24,67 @@ function startGame (){
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     setNextQuestion()
-
 }
 
+//Create a function to display next questions
 function setNextQuestion () {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer (){
-
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+          button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+      })
 }
+
+//Create function to reset to question with multiple answers once correct answer selected
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+  }
+
+function selectAnswer (e){
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide")
+  } else {
+    startButton.innerText = "Restart"
+    startButton.classList.remove("hide")
+  }
+}
+
+//Set class status to correct or wrong to change color based on answer selected
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add("correct")
+    } else {
+      element.classList.add("wrong")
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+  }
+  
 //Question Array
 var questions = [
     {
@@ -46,7 +106,7 @@ var questions = [
       ]
     },
     {
-      question: 'Select an example of comments in JS?',
+      question: 'Select an example of comments/psuedo code in JS?',
       answers: [
         { text: '<!--Hello--!>', correct: false },
         { text: '//Hello', correct: true },
@@ -55,7 +115,7 @@ var questions = [
       ]
     },
     {
-      question: 'What does HTML stand for',
+      question: 'What does HTML stand for?',
       answers: [
         { text: 'Hyper Type Median Language', correct: false },
         { text: 'Hyper Text Markup Language', correct: true },
@@ -108,7 +168,11 @@ var questions = [
 
 
 
+//   var highscoreButton = document.getElementById("highscore-btn")
+//   var homePage = document.getElementById("home")
 
+// highscoreButton.classList.add("hide")
+//     homePage.classList.add("hide")
 
 
 
