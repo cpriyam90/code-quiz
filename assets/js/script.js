99f2943e67219a9fd9a365ee9765633bacf832f5
Priyam Carto <cpriyam90@gmail.com>
2022-03-27 20:49:54 -0400

    syle sheet edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,133 @@
-var startBtn = document.getElementById("start-btn");
-var timer = document.getElementById("timer");
-var questionContainer = document.getElementById("question-container");
-var questionText = document.getElementById("question-text");
-var answerContainer = document.getElementById("answer-container");
-var nextBtn = document.getElementById("next-btn");
-var questions = [
-    {
-        question: "1+1", 
-        answers: [
-            "2", "4", "6", "10"
-        ],
-        correctAnswer: "2"
-    },
-    {
-        question: "2+2", 
-        answers: [
-            "2", "4", "6", "11"
-        ],
-        correctAnswer: "4"
-    },
-    {
-        question: "3+3", 
-        answers: [
-            "2", "4", "6", "12"
-        ],
-        correctAnswer: "6"
-    },
-];
+var startButton = document.getElementById("start-btn")
+var questionContainer = document.getElementById("questions-container")
+var highscoreButton = document.getElementById("highscore-btn")
+var homePage = document.getElementById("home")
 
-var questionsIndex = 0
+startButton.addEventListener("click",startGame)
 
-function showQuestion(Q){
-    for (var i =0; i < questions.length; i++){
-        // console.log(questions[i].question);
-        // console.log(questions[i].answers);
-        questionText.innerHTML = questions[i].question;
-        var answerslist = questions[i].answers;
-    }
-    for (var i=0; i < 4; i++){
-        var answerBtn = document.createElement("button");
-        answerBtn.textContent = answerslist[i];
-        answerContainer.appendChild(answerBtn);
-    }   
+
+function startGame (){
+    console.log("started")
+    startButton.classList.add("hide")
+    highscoreButton.classList.add("hide")
+    homePage.classList.add("hide")
+    questionContainer.classList.remove("hide")
+
 }
-showQuestion()
-var time = 30;
-var counter;
-function setTime(){
-    counter = setInterval(function(){
-        time--;
-        timer.textContent = time;
-        if (time==0){
-            clearInterval(time);
-        }
-    },1000)
+
+function setNextQuestion () {
+
+}
+
+function selectAnswer (){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var startBtn = document.getElementById("start-btn");
+// var timer = document.getElementById("timer");
+// var questionContainer = document.getElementById("question-container");
+// var questionText = document.getElementById("question-text");
+// var answerContainer = document.getElementById("answer-container");
+// var nextBtn = document.getElementById("next-btn");
+// var questions = [
+//     {
+//         question: "1+1", 
+//         answers: [
+//             "2", "4", "6", "10"
+//         ],
+//         correctAnswer: "2"
+//     },
+//     {
+//         question: "2+2", 
+//         answers: [
+//             "2", "4", "6", "11"
+//         ],
+//         correctAnswer: "4"
+//     },
+//     {
+//         question: "3+3", 
+//         answers: [
+//             "2", "4", "6", "12"
+//         ],
+//         correctAnswer: "6"
+//     },
+// ];
+
+// var questionsIndex = 0
+
+// function showQuestion(Q){
+//     for (var i =0; i < questions.length; i++){
+//         // console.log(questions[i].question);
+//         // console.log(questions[i].answers);
+//         questionText.innerHTML = questions[i].question;
+//         var answerslist = questions[i].answers;
+//     }
+//     for (var i=0; i < 4; i++){
+//         var answerBtn = document.createElement("button");
+//         answerBtn.textContent = answerslist[i];
+//         answerContainer.appendChild(answerBtn);
+//     }   
+// }
+// showQuestion()
+// var time = 30;
+// var counter;
+// function setTime(){
+//     counter = setInterval(function(){
+//         time--;
+//         timer.textContent = time;
+//         if (time==0){
+//             clearInterval(time);
+//         }
+//     },1000)
     
-}
-setTime();
+// }
+// setTime();
